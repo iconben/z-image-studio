2ed8fb9fb5cd7b8e805931e909c207f3330c0331
Benjamin Qin <benjamin.qin@gmail.com>
2026-01-11 16:43:34 +0900

    feat: macOS packaging and Homebrew Cask support

---
+++ homebrew/z-image-studio.rb
@@ -0,0 +1,22 @@
+cask "z-image-studio" do
+  version "VERSION_PLACEHOLDER"
+  sha256 "SHA256_PLACEHOLDER"
+
+  url "https://github.com/iconben/z-image-studio/releases/download/v#{version}/Z-Image-Studio-#{version}-macOS.dmg"
+  name "Z-Image Studio"
+  desc "Local AI image generation toolkit (CLI + Web UI)"
+  homepage "https://github.com/iconben/z-image-studio"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
+
+  app "Z-Image Studio.app"
+
+  zap trash: [
+    "~/Library/Application Support/com.z-image-studio.app",
+    "~/Library/Preferences/com.z-image-studio.app.plist",
+    "~/Library/Saved Application State/com.z-image-studio.app.savedState",
+  ]
+end
